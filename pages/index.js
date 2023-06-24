f4dd95d8e2fe6969227c1530cd9c8766d915b8b6
yungblud <killi8n@gmail.com>
2023-06-24 14:49:26 +0900

    feat: lint fix

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import Head from "next/head";
-import Link from "next/link";
-import { getDatabase } from "../lib/notion";
-import { Text } from "./[id].js";
-import styles from "./index.module.css";
+import Head from 'next/head'
+import Link from 'next/link'
+import { getDatabase } from '../lib/notion'
+import { Text } from './[id].js'
+import styles from './index.module.css'
 
-export const databaseId = process.env.NOTION_DATABASE_ID;
+export const databaseId = process.env.NOTION_DATABASE_ID
 
 export default function Home({ posts }) {
   return (
@@ -50,16 +50,16 @@ export default function Home({ posts }) {
           <h1>Next.js blog powered by Notion API</h1>
           <p>
             This is an example of a Next.js blog with data fetched with Notions
-            API. The data comes from{" "}
+            API. The data comes from{' '}
             <a href={`https://www.notion.so/${databaseId}`}>this table</a>. Get
-            the source code on{" "}
+            the source code on{' '}
             <a href="https://github.com/samuelkraft/notion-blog-nextjs">
               Github
-            </a>{" "}
-            or read{" "}
+            </a>{' '}
+            or read{' '}
             <a href="https://samuelkraft.com/blog/building-a-notion-blog-with-public-api">
               my blogpost
-            </a>{" "}
+            </a>{' '}
             on building your own.
           </p>
         </header>
@@ -68,13 +68,13 @@ export default function Home({ posts }) {
         <ol className={styles.posts}>
           {posts.map((post) => {
             const date = new Date(post.last_edited_time).toLocaleString(
-              "en-US",
+              'en-US',
               {
-                month: "short",
-                day: "2-digit",
-                year: "numeric",
+                month: 'short',
+                day: '2-digit',
+                year: 'numeric',
               }
-            );
+            )
             return (
               <li key={post.id} className={styles.post}>
                 <h3 className={styles.postTitle}>
@@ -86,21 +86,21 @@ export default function Home({ posts }) {
                 <p className={styles.postDescription}>{date}</p>
                 <Link href={`/${post.id}`}>Read post →</Link>
               </li>
-            );
+            )
           })}
         </ol>
       </main>
     </div>
-  );
+  )
 }
 
 export const getStaticProps = async () => {
-  const database = await getDatabase(databaseId);
+  const database = await getDatabase(databaseId)
 
   return {
     props: {
       posts: database,
     },
     revalidate: 1,
-  };
-};
+  }
+}
