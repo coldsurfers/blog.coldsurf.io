a123fbc8220bf42c4bb289429d3bec5b4e9cd685
yungblud <killi8n@gmail.com>
2023-11-26 21:45:47 +0900

    feat: implemented gtag

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 import { Inter } from 'next/font/google'
@@ -17,6 +18,19 @@ export default function RootLayout({ children }) {
         <meta
           name="google-site-verification"
           content="t8pam4eI0ydfgF_W2Js3Q9bdfCsbvZA83PSE2JDh1ww"
+        />
+        {/* <!-- Google tag (gtag.js) --> */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-VDP9GWZWGR"
+        />
+        <Script
+          content={`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-VDP9GWZWGR');`}
         />
       </head>
       <body className={inter.className}>{children}</body>
