d7dadebbf0911919277344c3c6370a273b797290
yungblud <killi8n@gmail.com>
2023-12-16 23:24:23 +0900

    chore: modified sitemap setup

--- app/(sitemaps)/server-sitemap.xml/route.js
+++ app/(sitemaps)/server-sitemap.xml/route.js
@@ -5,13 +5,18 @@ export async function GET() {
   // Method to source urls from cms
   // const urls = await fetch('https//example.com/api')
   const posts = await getInternalPosts()
-
-  return getServerSideSitemap(
-    posts.map((post) => ({
+  const sitemaps = [
+    {
+      loc: `${process.env.SITE_URL}`,
+      lastmod: new Date().toISOString(),
+    },
+    ...posts.map((post) => ({
       loc: `${process.env.SITE_URL}/article/${post.slug}`,
       lastmod: post.date.toISOString(),
-    }))
-  )
+    })),
+  ]
+
+  return getServerSideSitemap(sitemaps)
 
   // return getServerSideSitemap([
   //   {
